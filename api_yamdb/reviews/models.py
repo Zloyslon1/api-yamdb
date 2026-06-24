149f699c9bcc6d1e70051bb79ebf4d6b73eb5d75
mraksdev <a.krylov.work@gmail.com>
2026-06-24 15:04:20 +0300

    fix(models): убран magic number 2026, заменён на date.today().year

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator
 from django.db import models
@@ -92,7 +94,7 @@ class Title(models.Model):
     year = models.IntegerField(
         'Год выпуска',
         validators=(
-            MaxValueValidator(2026),
+            MaxValueValidator(date.today().year),
         ),
     )
     description = models.TextField(
