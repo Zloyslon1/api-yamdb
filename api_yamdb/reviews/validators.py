a5985f8c743d7d898166908ce794cac36e59674b
Zloyslon1 <92743244+Zloyslon1@users.noreply.github.com>
2026-06-28 22:40:59 +0500

    fix validators (#16)

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
 
 from reviews.constants import RESERVED_USERNAMES, VALIDATE_USERNAME_PATTERN
 
